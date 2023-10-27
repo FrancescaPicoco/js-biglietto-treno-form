380f69d09457541ac2ceda1bb2246828fa129337
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-10-27 15:41:23 +0200

    var value

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-let kilom = (parseInt("Inserisci i kilometri da percorrere"));
-let nam =("Inserisci il tuo nome e cognome");
 
+
+//document.getElementById('nome').value;
+//document.getElementById('km').value;
+
+function shared(confirm) {
+    var value =document.getElementById('nome').value;
+    var value = document.getElementById('km').value;
+    console.log(value);
+}
 
