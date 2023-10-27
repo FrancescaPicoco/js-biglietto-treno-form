b9006c2bd2cd82f32eee91b04ef5e71cdde91294
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-10-27 18:59:24 +0200

    prova 19.00

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 
-//document.getElementById('nome').value;
-//document.getElementById('km').value;
-let txt = document.getElementById("container");
+let txt = document.getElementById("main");
             txt.style.textAlign = "center";
 
 
@@ -10,11 +8,30 @@ let txt = document.getElementById("container");
 //document.getElementById("container").style.lineHeight = "3";
 
 function shared(confirm) {
-    var value =document.getElementById('nome').value;
-    console.log(value);
-    var value = document.getElementById('km').value;
-    console.log(value);
-    var value = document.getElementById('old').value;
-    console.log=(value);
-}
+    const value0 =document.getElementById('nome').value;
+    console.log(value0);
+    let value1 = document.getElementById('km').value;
+    console.log(value1);
+    const value2 = document.getElementById('old').value;
+    console.log(value2);
 
+let fullprice = (value1 * 0.21);
+if ( isNaN (value2) || isNaN(value1) ){
+    document.getElementById("hide-cont").innerHTML = `Error`;
+    console.log(`i caratteri non sono numerici`);   
+}else if( value2 < 18) {
+    const saleUnder=((value1 * 20) / 100);
+    document.getElementById("price").innerHTML = `${saleUnder}`;
+    document.getElementById("offert").innerHTML = `Biglietto scontato under18`;
+    console.log("costo scontato" , saleUnder);
+    } else if( value2 > 65) {
+    const saleOver=((value1 * 40) / 100);
+    document.getElementById("price").innerHTML = `${saleOver}`;
+    document.getElementById("offert").innerHTML = `Biglietto scontato over65`;
+    console.log("costo scontato" , saleOver);
+    }
+    else {
+    document.getElementById("mex").innerHTML = `Prezzo: ${fullprice}`;
+    console.log("prezzo pieno" , fullprice);
+    }
+}
